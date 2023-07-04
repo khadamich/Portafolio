45b35feb957ab45eced9b876ea03da31cfdefa2c
Michell Martinez <132023832+khadamich@users.noreply.github.com>
2023-07-04 11:24:47 -0600

    Add files via upload

---
+++ Monte-Carlo/FUENTES/CMakeFiles/DMX.dir/DependInfo.cmake
@@ -0,0 +1,63 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/michell/Documents/NaI/MC/NaI_DMnu_3/DMX.cc" "/Users/michell/Documents/NaI/MC/FUENTES/CMakeFiles/DMX.dir/DMX.cc.o"
+  "/Users/michell/Documents/NaI/MC/NaI_DMnu_3/src/DMXActionInitializer.cc" "/Users/michell/Documents/NaI/MC/FUENTES/CMakeFiles/DMX.dir/src/DMXActionInitializer.cc.o"
+  "/Users/michell/Documents/NaI/MC/NaI_DMnu_3/src/DMXDetectorConstruction.cc" "/Users/michell/Documents/NaI/MC/FUENTES/CMakeFiles/DMX.dir/src/DMXDetectorConstruction.cc.o"
+  "/Users/michell/Documents/NaI/MC/NaI_DMnu_3/src/DMXEventAction.cc" "/Users/michell/Documents/NaI/MC/FUENTES/CMakeFiles/DMX.dir/src/DMXEventAction.cc.o"
+  "/Users/michell/Documents/NaI/MC/NaI_DMnu_3/src/DMXEventActionMessenger.cc" "/Users/michell/Documents/NaI/MC/FUENTES/CMakeFiles/DMX.dir/src/DMXEventActionMessenger.cc.o"
+  "/Users/michell/Documents/NaI/MC/NaI_DMnu_3/src/DMXMaxTimeCuts.cc" "/Users/michell/Documents/NaI/MC/FUENTES/CMakeFiles/DMX.dir/src/DMXMaxTimeCuts.cc.o"
+  "/Users/michell/Documents/NaI/MC/NaI_DMnu_3/src/DMXMinEkineCuts.cc" "/Users/michell/Documents/NaI/MC/FUENTES/CMakeFiles/DMX.dir/src/DMXMinEkineCuts.cc.o"
+  "/Users/michell/Documents/NaI/MC/NaI_DMnu_3/src/DMXParticleSource.cc" "/Users/michell/Documents/NaI/MC/FUENTES/CMakeFiles/DMX.dir/src/DMXParticleSource.cc.o"
+  "/Users/michell/Documents/NaI/MC/NaI_DMnu_3/src/DMXParticleSourceMessenger.cc" "/Users/michell/Documents/NaI/MC/FUENTES/CMakeFiles/DMX.dir/src/DMXParticleSourceMessenger.cc.o"
+  "/Users/michell/Documents/NaI/MC/NaI_DMnu_3/src/DMXPhysicsList.cc" "/Users/michell/Documents/NaI/MC/FUENTES/CMakeFiles/DMX.dir/src/DMXPhysicsList.cc.o"
+  "/Users/michell/Documents/NaI/MC/NaI_DMnu_3/src/DMXPmtHit.cc" "/Users/michell/Documents/NaI/MC/FUENTES/CMakeFiles/DMX.dir/src/DMXPmtHit.cc.o"
+  "/Users/michell/Documents/NaI/MC/NaI_DMnu_3/src/DMXPmtSD.cc" "/Users/michell/Documents/NaI/MC/FUENTES/CMakeFiles/DMX.dir/src/DMXPmtSD.cc.o"
+  "/Users/michell/Documents/NaI/MC/NaI_DMnu_3/src/DMXPrimaryGeneratorAction.cc" "/Users/michell/Documents/NaI/MC/FUENTES/CMakeFiles/DMX.dir/src/DMXPrimaryGeneratorAction.cc.o"
+  "/Users/michell/Documents/NaI/MC/NaI_DMnu_3/src/DMXRunAction.cc" "/Users/michell/Documents/NaI/MC/FUENTES/CMakeFiles/DMX.dir/src/DMXRunAction.cc.o"
+  "/Users/michell/Documents/NaI/MC/NaI_DMnu_3/src/DMXRunActionMessenger.cc" "/Users/michell/Documents/NaI/MC/FUENTES/CMakeFiles/DMX.dir/src/DMXRunActionMessenger.cc.o"
+  "/Users/michell/Documents/NaI/MC/NaI_DMnu_3/src/DMXScintHit.cc" "/Users/michell/Documents/NaI/MC/FUENTES/CMakeFiles/DMX.dir/src/DMXScintHit.cc.o"
+  "/Users/michell/Documents/NaI/MC/NaI_DMnu_3/src/DMXScintSD.cc" "/Users/michell/Documents/NaI/MC/FUENTES/CMakeFiles/DMX.dir/src/DMXScintSD.cc.o"
+  "/Users/michell/Documents/NaI/MC/NaI_DMnu_3/src/DMXSpecialCuts.cc" "/Users/michell/Documents/NaI/MC/FUENTES/CMakeFiles/DMX.dir/src/DMXSpecialCuts.cc.o"
+  "/Users/michell/Documents/NaI/MC/NaI_DMnu_3/src/DMXStackingAction.cc" "/Users/michell/Documents/NaI/MC/FUENTES/CMakeFiles/DMX.dir/src/DMXStackingAction.cc.o"
+  "/Users/michell/Documents/NaI/MC/NaI_DMnu_3/src/DMXStackingActionMessenger.cc" "/Users/michell/Documents/NaI/MC/FUENTES/CMakeFiles/DMX.dir/src/DMXStackingActionMessenger.cc.o"
+  "/Users/michell/Documents/NaI/MC/NaI_DMnu_3/src/DMXSteppingAction.cc" "/Users/michell/Documents/NaI/MC/FUENTES/CMakeFiles/DMX.dir/src/DMXSteppingAction.cc.o"
+  "/Users/michell/Documents/NaI/MC/NaI_DMnu_3/src/DMXSteppingActionMessenger.cc" "/Users/michell/Documents/NaI/MC/FUENTES/CMakeFiles/DMX.dir/src/DMXSteppingActionMessenger.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "DMXENV_GPS_USE"
+  "G4INTY_USE_QT"
+  "G4INTY_USE_XT"
+  "G4UI_USE"
+  "G4UI_USE_QT"
+  "G4UI_USE_TCSH"
+  "G4VERBOSE"
+  "G4VIS_USE"
+  "G4VIS_USE_OPENGL"
+  "G4VIS_USE_OPENGLQT"
+  "G4VIS_USE_RAYTRACERX"
+  "G4_STORE_TRAJECTORY"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/Users/michell/Documents/NaI/MC/NaI_DMnu_3/include"
+  "/Users/michell/Documents/NaI/geant4.10.03.p03/include/Geant4"
+  "/usr/local/lib/QtCore.framework"
+  "/usr/local/lib/QtCore.framework/Headers"
+  "/usr/local/include/QtGui"
+  "/usr/local/include/QtOpenGL"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
